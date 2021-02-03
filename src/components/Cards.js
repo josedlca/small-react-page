65e360f32748453b94999afb311f0a1c5a4a887d
josedlca <jose.dlcruz.ayala@gmail.com>
2021-02-03 16:37:20 -0500

    footer and 3 pages more

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -9,7 +9,38 @@ function Cards() {
             <div className="cards__container">
                 <div className="cards__wrapper">
                     <ul className="cards__items">
-                        <CardItem />
+                        <CardItem 
+                            src= 'images/img-9.jpg'
+                            text= 'Explore the hidden waterfall deepinside the Amazon Jungle'
+                            label= 'Adventure'
+                            path= '/services'
+                        />
+                        <CardItem 
+                            src= 'images/img-2.jpg'
+                            text= 'Travel through the Islands of Bali in a Pribate Cruise'
+                            label= 'Luxury'
+                            path= '/services'
+                        />
+                    </ul>
+                    <ul className="cards__items">
+                        <CardItem 
+                            src= 'images/img-9.jpg'
+                            text= 'Explore the hidden waterfall deepinside the Amazon Jungle'
+                            label= 'Adventure'
+                            path= '/services'
+                        />
+                        <CardItem 
+                            src= 'images/img-2.jpg'
+                            text= 'Travel through the Islands of Bali in a Pribate Cruise'
+                            label= 'Luxury'
+                            path= '/services'
+                        />
+                        <CardItem 
+                            src= 'images/img-9.jpg'
+                            text= 'Explore the hidden waterfall deepinside the Amazon Jungle'
+                            label= 'Adventure'
+                            path= '/services'
+                        />
                     </ul>
                 </div>
             </div>
